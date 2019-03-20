a2e6ff23d52236beee67d6cab42c0ab17e56f7a0
mori <k.andar.m.p@gmail.com>
2019-03-20 15:54:21 +0900

    ストップウォッチ作成中

--- stopwatch/js/main.js
+++ stopwatch/js/main.js
@@ -9,6 +9,7 @@
   var startTime;
   var elapsedTime = 0;
   var timerId;
+  var timeToAdd = 0;
 
   function updateTimerText(){
     var m = Math.floor(elapsedTime / 60000);
@@ -24,7 +25,8 @@
 
   function countUp(){
     timerId = setTimeout(function(){
-      elapsedTime = Date.now() - startTime;
+      // elapsedTime = Date.now() - startTime;
+      elapsedTime = Date.now() - startTime + timeToAdd;
       // console.log(elapsedTime);
       updateTimerText();
       countUp();
@@ -37,9 +39,11 @@
   });
   stop.addEventListener('click', function(){
     clearTimeout(timerId);
+    timeToAdd += Date.now() - startTime;
   });
   reset.addEventListener('click', function(){
     elapsedTime = 0;
+    timeToAdd = 0;
     updateTimerText();
   });
 })();
